c1c706c81d7a472aad92e68e59c716e6e59ee479
edufrasson <edudomine2016@gmail.com>
2023-08-24 14:17:27 -0300

    feat: criacao dos arquivos

--- App/Controller/ProdutoController.php
+++ App/Controller/ProdutoController.php
@@ -1,24 +1,62 @@
 <?php
 namespace App\Controller;
 
+use App\Model\ProdutoModel;
+
 class ProdutoController extends Controller {
-	public static function insert() 
-	{
+	public static function index()
+    {
+        parent::isAuthenticated();
 
-	}
+        $model = new ProdutoModel();
+        $model->getAllRows();
 
-	public static function select() 
-	{
+        include 'View/modules/Produto/ListarProduto.php';
+    }
 
-	}
+    public static function getAll(){
+        parent::isAuthenticated();
 
-	public static function update() 
-	{
+        $model = new ProdutoModel();
+        $model->getAllRows();
+       
+        parent::setResponseAsJSON($model->rows);
+    }
 
-	}
+    public static function getById()
+    {   
+        parent::isAuthenticated();
+        
+        $model = new ProdutoModel();
+        $data = $model->getById($_GET['id']);
 
-	public static function delete() 
-	{
+        parent::setResponseAsJSON($data);
+    }
 
-	}
+    public static function save()
+    {   
+        parent::isAuthenticated();
+     
+        $model = new ProdutoModel();
+
+        $model->id = $_POST['id'];
+		$model->descricao = $_POST['descricao'];
+		$model->preco = $_POST['preco'];
+		$model->id_categoria = $_POST['id_categoria'];
+            
+        $model->save();
+
+        parent::setResponseAsJSON($model);
+    }
+
+    public static function delete()
+    {   
+        parent::isAuthenticated();
+     
+        $model = new ProdutoModel();
+
+        $model->delete( (int) $_GET['id']);
+
+        parent::setResponseAsJSON($model);
+    }
 }
